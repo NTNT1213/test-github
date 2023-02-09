d2ceb7b718610c1907f0fba8bc1d3d901cfe8cd5
NTNT1213 <124746627+NTNT1213@users.noreply.github.com>
2023-02-09 23:48:49 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Bai1;
+
+import Bai2.TestComplex;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner s=new Scanner((System.in));
+        TestPoint t= new TestPoint();
+        TestComplex tc =new TestComplex();
+        t.Test();
+        tc.Test();
+    }
+}
